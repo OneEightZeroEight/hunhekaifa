b0cc817477318c4d41d1155db6f5f0b2b3a91733
wscats <2587575267@qq.com>
2018-12-13 19:28:40 +0800

    混合开发

---
+++ 1808/1808/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1808
+//
+//  Created by Eno on 2018/12/13.
+//  Copyright © 2018 Eno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ lemon/my-project/src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Xheader/>
+        <Xnav />
+    </div>
+</template>
+<script>
+import Xheader from '../components/Xheader.vue'
+import Xnav from '../components/Xnav.vue'
+export default {
+  components: {
+    Xheader,
+    Xnav
+  }
+}
+</script>
